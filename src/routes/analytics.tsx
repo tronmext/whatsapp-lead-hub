fbdcd6bdfccd6226c6859c9c55469ecd8d92add1
Guilherme Giorgi <guilherme@giorgi.com.br>
2026-05-15 04:17:44 -0400

    fix: remove duplicate createFileRoute import in analytics

--- src/routes/analytics.tsx
+++ src/routes/analytics.tsx
@@ -1,4 +1,3 @@
-import { createFileRoute } from '@tanstack/react-router'
 import { createFileRoute } from "@tanstack/react-router";
 import { useMemo } from "react";
 import { useServerFn } from "@tanstack/react-start";
